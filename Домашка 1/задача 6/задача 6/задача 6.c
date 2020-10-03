02e94470bf0b6928daff0b5fcac123262894fbfb
MikePuzanov <mikepuzanov@mail.ru>
2020-10-03 17:29:13 +0300

    Домашка 1

---
+++ Домашка 1/задача 6/задача 6/задача 6.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void transposition(int leftEnd, int rightEnd, int array[])
+{
+    for (int i = leftEnd; i < (leftEnd + ((rightEnd - leftEnd) / 2)); ++i)
+        {
+            array[i] = array[i] + array[rightEnd + leftEnd - 1 - i];
+            array[rightEnd + leftEnd - 1 - i] = array[i] - array[rightEnd + leftEnd - 1 - i];
+            array[i] = array[i] - array[rightEnd + leftEnd - 1 - i];
+        }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n = 10;
+    int m = 5;
+    printf(" Введите m и n: ");
+    scanf("%i %i", &m, &n);
+    int array[100];
+    printf(" Введите массив: ");
+    for (int i = 0; i < m + n; ++i)
+    {
+        scanf(" %i", &array[i]);
+    }
+    transposition(0, m, array);
+    transposition(m , m + n, array);
+    transposition(0, m + n, array);
+    printf(" Изменёный массив: ");
+    for (int i = 0; i < m + n; ++i)
+    {
+        printf("%i ", array[i]);
+    }
+}
