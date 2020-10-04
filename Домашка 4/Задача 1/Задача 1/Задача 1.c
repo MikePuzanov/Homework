3acdeaa1a438bb353ffd000d83b9d1ebe4051d24
MikePuzanov <mikepuzanov@mail.ru>
2020-10-04 12:15:46 +0300

    Создание проекта.

---
+++ Домашка 4/Задача 1/Задача 1/Задача 1.c
@@ -0,0 +1,14 @@
+﻿#include "Header.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int main()
+{
+    int firstElement = 0;
+    int secondElement = 0;
+    scanf("%i %i", &firstElement, &secondElement);
+
+}
