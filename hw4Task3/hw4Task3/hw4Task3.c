f14fd349ba9f86c0f02a60aa4aa4bfb6dd56a155
MikePuzanov <mikepuzanov@mail.ru>
2020-10-11 11:39:02 +0300

    Написание шапки и добавление вспомогательных файлов

---
+++ hw4Task3/hw4Task3/hw4Task3.c
@@ -0,0 +1,40 @@
+﻿#include "Header.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int decide = 0;
+    while (decide != -1)
+    {
+        scanf("%i", &decide);
+        switch (decide)
+        {
+            case 0:
+                decide = -1;
+            break;
+            case 1:
+
+            break;
+            case 2:
+
+            break;
+            case 3:
+
+            break;
+            case 4:
+
+            break;
+            case 5:
+
+            break;
+        default:
+            printf("Введите число от 0 до 5!!!\n");
+            break;
+        }
+    }
+}
