2b88ed339ed957c45a4588b584c28b9ccd74f8b8
MikePuzanov <mikepuzanov@mail.ru>
2020-10-27 00:59:10 +0300

    подключение библиотек

--- hw5Task3/hw5Task3/hw5Task3.c
+++ hw5Task3/hw5Task3/hw5Task3.c
@@ -1,4 +1,13 @@
-﻿#include <stdio.h>
+﻿#include "../../hw5Stack/hw5Stack/Stack.h"
+#include "../../hw5Stack/hw5Stack/TestStack.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 int main()
 {
