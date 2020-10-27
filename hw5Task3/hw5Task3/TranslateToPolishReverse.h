44de9a4e4e8270dc53e8fea1cdf59ea667ac7023
MikePuzanov <mikepuzanov@mail.ru>
2020-10-27 19:32:17 +0300

    Добавление комментариев к функциям

--- hw5Task3/hw5Task3/TranslateToPolishReverse.h
+++ hw5Task3/hw5Task3/TranslateToPolishReverse.h
@@ -1,13 +1,13 @@
 #pragma once
 
-//
+// Возвращает опературу его приоритет
 int priorityOperator(char symbol);
 
-//
+// Возвращает опературу его id
 int idOperator(char symbol);
 
-//
+// Возвращает оператор по id
 char fromIdToOpertor(int idSymbol);
 
-//
+// Функция выполняющая перевод в потсфиксную форму
 void translateToPolishReverse(struct StackElement* head, char line[], char expression[], int* index);
