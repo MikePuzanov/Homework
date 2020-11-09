69985c49ac2adf66bae95f4313b3acda4e1c4932
MikePuzanov <mikepuzanov@mail.ru>
2020-11-09 20:40:26 +0300

    resolve conflict

--- hw4Task3/hw4Task3/phoneNumberFunction.h
+++ hw4Task3/hw4Task3/phoneNumberFunction.h
@@ -10,11 +10,7 @@ struct PhoneBook
 };
 
 // проверяет количество номеров в базе
-<<<<<<< HEAD
-bool checkNumberOfPerson(int index);
-=======
 bool isPhonebookFull(int index);
->>>>>>> 842dbac9b8c196227204ee25f50bcd4f2e90a675
 
 // поиск имени по номеру телефона
 bool nameSearchByPhone(struct PhoneBook person[], int index, char phone[], int* indexHelp);
@@ -23,15 +19,6 @@ bool nameSearchByPhone(struct PhoneBook person[], int index, char phone[], int* 
 bool phoneSearchByName(struct PhoneBook person[], int index, char name[], int* indexHelp);
 
 // Проверяет есть ли введенное имя в базе
-<<<<<<< HEAD
-bool checkName(struct PhoneBook person[], char name[], int index);
-
-// Проверяет есть ли введенный телефон в базе
-bool checkPhone(struct PhoneBook person[], char phone[], int index);
-
-// функция для добавления номеров в файл
-void printInFile(struct PhoneBook person[], int index, char* file);
-=======
 bool doesNameExist(struct PhoneBook person[], char name[], int index);
 
 // Проверяет есть ли введенный телефон в базе
@@ -42,4 +29,3 @@ void printInFile(struct PhoneBook person[], int index, char* file);
 
 // Загрузка из файла
 void readFromFile(struct PhoneBook person[], int* index, char* file);
->>>>>>> 842dbac9b8c196227204ee25f50bcd4f2e90a675
