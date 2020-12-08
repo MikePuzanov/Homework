29e7a9962d0192b589926859efdeae7fb74cff63
MikePuzanov <mikepuzanov@mail.ru>
2020-12-08 15:13:54 +0300

    /

---
+++ Test2.2Task2/Test2.2Task2/Test2.2Task2.c
@@ -0,0 +1,18 @@
+﻿#include "List.h"
+#include "Test.h"
+
+#include <stdio.h>
+
+int main()
+{
+    if (!testList())
+    {
+        return 4;
+    }
+    List* head = NULL;
+    readFromFile("File.txt", &head);
+    List* newList = getNewList(head);
+    printList(newList);
+    deleteList(&head);
+    deleteList(&newList);
+}
