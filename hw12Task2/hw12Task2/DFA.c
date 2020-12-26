432ccb06a096c17ded06742f648dd62eaf65e8c6
MikePuzanov <mikepuzanov@mail.ru>
2020-12-26 23:22:42 +0300

    DFA

---
+++ hw12Task2/hw12Task2/DFA.c
@@ -0,0 +1,134 @@
+#include "DFA.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define ANY 0
+#define SLASH 1
+#define STAR 2
+#define STATESNUMBER 5
+#define SYMBOLSNUMBER 3
+
+enum STATE
+{
+	notInComment,
+	forwardSlashStart,
+	starStart,
+	starEnd,
+	forwardSlashEnd
+};
+
+int** readStateTable(const char* fileName)
+{
+	FILE* table = fopen(fileName, "r");
+	int** statesTable = calloc(STATESNUMBER, sizeof(int*));
+	for (int i = 0; i < STATESNUMBER; i++)
+	{
+		statesTable[i] = calloc(SYMBOLSNUMBER, sizeof(int));
+	}
+	for (int i = 0; i < STATESNUMBER; i++)
+	{
+		for (int j = 0; j < SYMBOLSNUMBER; j++)
+		{
+			fscanf(table, "%i", &statesTable[i][j]);
+		}
+	}
+	fclose(table);
+	return statesTable;
+}
+
+void deleteTable(int** statesTable)
+{
+	for (int i = 0; i < STATESNUMBER; i++)
+	{
+		free(statesTable[i]);
+	}
+	free(statesTable);
+}
+
+char* DFA(const char* string, int** statesTable, bool* isInComment)
+{
+	char* answer = calloc(strlen(string) + 1, sizeof(char));
+	int state = *isInComment ? starStart : 0;
+	int index = 0;
+	while (true)
+	{
+		char token = string[index];
+		if (token == '\0' || token == '\n')
+		{
+			answer[strlen(answer) + 1] = '\0';
+			return answer;
+		}
+		switch (state)
+		{
+		case notInComment:
+			if (token == '/')
+			{
+				state = statesTable[state][SLASH];
+				break;
+			}
+		break;
+		case forwardSlashStart:
+			if (token == '*')
+			{
+				state = statesTable[state][STAR];
+				strcat(answer, "/*");
+				*isInComment = true;
+				break;
+			}
+			else if (token != '/')
+			{
+				state = statesTable[state][ANY];
+			}
+		break;
+		case starStart:
+			answer[strlen(answer)] = token;
+			if (token == '*')
+			{
+				state = statesTable[state][STAR];
+			}
+			else if (token == '/')
+			{
+				state = statesTable[state][SLASH];
+			}
+			else
+			{
+				state = statesTable[state][ANY];
+			}
+		break;
+		case starEnd:
+			answer[strlen(answer)] = token;
+			if (token == '*')
+			{
+				state = statesTable[state][STAR];
+			} 
+			else if (token == '/')
+			{
+				state = statesTable[state][SLASH];
+				*isInComment = false;
+			}
+			else
+			{
+				state = statesTable[state][ANY];
+			}
+		break;
+		case forwardSlashEnd:
+			if (token == '*')
+			{
+				state = statesTable[state][STAR];
+			}
+			else if (token == '/')
+			{
+				state = statesTable[state][SLASH];
+			}
+			else
+			{
+				state = statesTable[state][ANY];
+			}
+		break;
+		}
+		index++;
+	}
+}
---
+++ hw12Task2/hw12Task2/hw12Task2.c
@@ -0,0 +1,35 @@
+﻿#include "DFA.h"
+#include "Test.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	if (!test())
+	{
+		printf("Тест не пройден!\n");
+		return 5;
+	}
+	printf("Тест пройден!\n\n");
+	int** statesTable = readStateTable("StateTable.txt");
+	FILE* file = fopen("File.txt", "r");
+	if (file == NULL)
+	{
+		return 1;
+	}
+	bool isInComment = false;
+	while (!feof(file))
+	{
+		char string[1000] = "";
+		fgets(string, 1000, file);
+		char* answer = DFA(string, statesTable, &isInComment);
+		printf("%s\n", answer);
+		free(answer);
+	}
+	fclose(file);
+	deleteTable(statesTable);
+}
