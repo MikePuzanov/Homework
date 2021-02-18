83de5076c071994640aa204a2dee6b13a7fcf942
MikePuzanov <mikepuzanov@mail.ru>
2021-02-19 01:54:32 +0300

    conflict

--- hw4Task3/hw4Task3/phoneNumberFunction.h
+++ hw4Task3/hw4Task3/phoneNumberFunction.h
@@ -28,8 +28,4 @@ bool doesPhoneExist(struct PhoneBook person[], char phone[], int index);
 void printInFile(struct PhoneBook person[], int index, char* file);
 
 // Загрузка из файла
-<<<<<<< HEAD
 void readFromFile(struct PhoneBook person[], int* index, char* file);
-=======
-void readFromFile(struct PhoneBook person[], int* index, char* file);
->>>>>>> hw5Stack
